593429d2fce772ba6e1f5b451a222b84cb3aba4f
Austin Godber <godber@uberhip.com>
2020-10-13 17:48:20 -0700

    added updateExecutions

--- src/teraslice-stats.ts
+++ src/teraslice-stats.ts
@@ -1,6 +1,5 @@
 import got from 'got';
 
-// TODO: Get logging in here somehow
 // TODO: move these interfaces out into their own file
 
 interface TerasliceInfo {
@@ -15,37 +14,57 @@ interface TerasliceInfo {
   teraslice_version: string
 }
 
+/**
+ * These are all in ms
+ */
 interface TerasliceQueryDuration {
+  controllers: number,
+  executions: number,
   info: number,
   jobs: number,
-  controllers: number
 }
 
 interface TerasliceStatsInterface {
     baseUrl: URL,
+    controllers: any[],
+    executions: any[],
     info: TerasliceInfo,
     jobs: any[],
-    controllers: any[]
     queryDuration: TerasliceQueryDuration
+}
+
+/** promisified setTimeout */
+export function pDelay<T = undefined>(delay = 1, arg?: T): Promise<T> {
+  return new Promise<T>((resolve) => {
+    setTimeout(resolve, delay, arg);
+  });
 }
 
 export default class TerasliceStats implements TerasliceStatsInterface {
   baseUrl: URL;
 
+  controllers: any[];
+
+  executions: any[];
+
   info: any;
 
   jobs: any[];
-
-  controllers: any[];
 
   queryDuration: TerasliceQueryDuration;
 
   constructor(baseUrl:string) {
     this.baseUrl = new URL(baseUrl);
-    this.jobs = [];
     this.controllers = [];
+    this.executions = [];
+    this.jobs = [];
     // FIXME: should I use something other than 0 here?
-    this.queryDuration = { info: 0, jobs: 0, controllers: 0 };
+    this.queryDuration = {
+      controllers: 0,
+      executions: 0,
+      info: 0,
+      jobs: 0,
+    };
   }
 
   async getTerasliceApi(path:string) {
@@ -77,12 +96,37 @@ export default class TerasliceStats implements TerasliceStatsInterface {
     return r;
   }
 
+  async updateExecutions() {
+    const time = process.hrtime();
+    this.executions = [];
+    const maxConcurrency = 10;
+
+    for (let i = 0; i < this.controllers.length; i += maxConcurrency) {
+      const controllersSlice = this.controllers.slice(i, i + maxConcurrency);
+
+      // eslint-disable-next-line no-await-in-loop
+      const r = await Promise.all(
+        controllersSlice.map((x) => this.getTerasliceApi(`/v1/ex/${x.ex_id}`)),
+      );
+      this.executions = this.executions.concat(r.map((x) => x.data));
+      // eslint-disable-next-line no-await-in-loop
+      await pDelay(25);
+    }
+
+    const NS_PER_SEC = 1e9;
+    const diff = process.hrtime(time);
+    this.queryDuration.executions = (diff[0] * NS_PER_SEC + diff[1]) / 1e6;
+  }
+
+  // FIXME: I don't think /jobs is even relevant here ... what I should really
+  // do is get the ex_ids from the controllers, and then go get the ex for each
+  // one of those.  Jobs are mostly irrelevant in this context.
+
   // I think I've been doing this sort of thing wrong in the past.
   // https://stackoverflow.com/questions/45285129/any-difference-between-await-promise-all-and-multiple-await
   async update() {
-    // eslint-disable-next-line no-console
+    // TODO: hard coding querySize is dumb
     const querySize = 200;
-    // eslint-disable-next-line func-names
     const run = async () => {
       const [info, jobs, controllers] = await Promise.all([
         this.getTerasliceApi('/'),
@@ -92,27 +136,14 @@ export default class TerasliceStats implements TerasliceStatsInterface {
       this.info = info.data;
       this.jobs = jobs.data;
       this.controllers = controllers.data;
-      this.queryDuration = {
-        info: info.queryDuration,
-        jobs: jobs.queryDuration,
-        controllers: controllers.queryDuration,
-      };
+      await this.updateExecutions();
+
+      this.queryDuration.info = info.queryDuration;
+      this.queryDuration.jobs = jobs.queryDuration;
+      this.queryDuration.controllers = controllers.queryDuration;
     };
     await run().catch((err) => {
-      // eslint-disable-next-line no-console
-      console.log(err);
-      // logger.error('Error updating Teraslice Info', err);
+      throw new Error(`Error caught on run() ${this.baseUrl}: ${err}`);
     });
   }
 }
-
-// async function main() {
-//   const terasliceStats = new TerasliceStats('http://ts-prod3.tera1.lan');
-//   await terasliceStats.update();
-
-//   console.log(`info: ${JSON.stringify(terasliceStats.info, null, 2)}`);
-//   console.log(`jobs: ${JSON.stringify(terasliceStats.jobs[0], null, 2)}`);
-//   console.log(`controllers: ${JSON.stringify(terasliceStats.controllers[0], null, 2)}`);
-// }
-
-// main();
